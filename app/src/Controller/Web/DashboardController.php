310120282b1f2e0de4380d966b9d9885f9ef6929
Sergio Neira Vargas <sh.neiravargas@gmail.com>
2023-12-18 20:48:27 -0300

    Update: elementos del menú de usuario, acciones predefinidas

--- app/src/Controller/Web/DashboardController.php
+++ app/src/Controller/Web/DashboardController.php
@@ -5,11 +5,16 @@ declare(strict_types=1);
 namespace App\Controller\Web;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -31,5 +36,18 @@ class DashboardController extends AbstractDashboardController
 
         yield MenuItem::linkToCrud('Users', 'fa fa-users', User::class)
             ->setPermission(User::ROLE_ADMIN);
+    }
+
+    public function configureUserMenu(UserInterface $user): UserMenu
+    {
+        return parent::configureUserMenu($user)
+            ->displayUserAvatar(false)
+            ->displayUserName(false);
+    }
+
+    public function configureActions(): Actions
+    {
+        return parent::configureActions()
+            ->add(Crud::PAGE_INDEX, Action::DETAIL);
     }
 }
