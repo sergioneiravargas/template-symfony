b8c299aa32391d0623015cc917efc8a292d9ed23
Sergio Neira Vargas <sh.neiravargas@gmail.com>
2023-12-17 22:33:56 -0300

    Update: csfix

--- app/src/Controller/Web/LoginController.php
+++ app/src/Controller/Web/LoginController.php
@@ -6,7 +6,6 @@ namespace App\Controller\Web;
 
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
